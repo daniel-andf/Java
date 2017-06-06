fd322353d34ab7149808411b88defde22b7af68f
daniel-andf <daniel.andf@gmail.com>
2017-06-05 19:06:25 -0700

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dandradefonsec00
+ */
+// Fig. 7.9: Card.java
+// Card class represents a playing card.
+
+public class Card 
+{
+   private final String face; // face of card ("Ace", "Deuce", ...)
+   private final String suit; // suit of card ("Hearts", "Diamonds", ...)
+
+   // two-argument constructor initializes card's face and suit
+   public Card(String face, String suit)
+   {
+      this.face = face;
+      this.suit = suit; 
+   } 
+
+   // return String representation of Card
+   public String toString() 
+   { 
+      return face + " of " + suit;
+   } 
+   
+} // end class Card
+
